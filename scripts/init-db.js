92b04e987b78b2a1ab3075d07fee7e0d161e9d22
embbnux <embbnux@embbnux.com>
2021-05-21 14:15:46 +0800

    add user table resource

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const { RCWebhook } = require('../src/app/models/rc-webhook');
 const { TrelloWebhook } = require('../src/app/models/trello-webhook');
+const { TrelloUser } = require('../src/app/models/trello-user');
 
 async function initDB() {
   await RCWebhook.sync();
   await TrelloWebhook.sync();
+  await TrelloUser.sync();
 }
 
 initDB();
