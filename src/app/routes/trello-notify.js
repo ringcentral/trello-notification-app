1f268fb147a08242e0837fdf64d4245596239ecf
Embbnux Ji <embbnux@embbnux.com>
2021-12-02 23:17:04 +0800

    chore: remove unused code

--- src/app/routes/trello-notify.js
+++ src/app/routes/trello-notify.js
@@ -4,35 +4,7 @@ const { TrelloWebhook } = require('../models/trello-webhook');
 const { TrelloUser } = require('../models/trello-user');
 const { getFilterId } = require('../lib/filter');
 const { Trello } = require('../lib/Trello');
-// const { formatGlipWebhookCardMessage } = require('../lib/formatMessage');
 const { formatAdaptiveCardMessage, CARD_TYPES } = require('../lib/formatAdaptiveCardMessage');
-
-// async function notify(req, res) {
-//   const trelloWebhookId = req.params.id;
-//   // console.log(JSON.stringify(req.body, null, 2));
-//   try {
-//     const trelloWebhook = await TrelloWebhook.findByPk(trelloWebhookId);
-//     if (!trelloWebhook) {
-//       throw new Error('trello webhook id not found!');
-//     }
-//     const filterId = getFilterId(req.body, trelloWebhook.config.filters);
-//     if (filterId) {
-//       const glipMessage = formatGlipWebhookCardMessage(req.body);
-//       await axios.post(trelloWebhook.rc_webhook_id, glipMessage, {
-//         headers: {
-//           Accept: 'application/json',
-//           'Content-Type': 'application/json'
-//         }
-//       });
-//     }
-//   } catch (e) {
-//     console.error(e)
-//   }
-//   res.status(200);
-//   res.json({
-//     result: 'OK',
-//   });
-// }
 
 async function updateBoardLabels(trello, trelloWebhook) {
   const labels = await trello.getLabels(trelloWebhook.config.boardId);
