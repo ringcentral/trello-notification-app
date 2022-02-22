d519b504912d5eef541fbf83199f400832c47cd9
Embbnux Ji <embbnux@embbnux.com>
2022-02-22 15:07:29 +0800

    chore: add notification webhook creatation test

--- test/webhook.test.js
+++ test/webhook.test.js
@@ -10,7 +10,7 @@ const { RCWebhook } = require('../src/app/models/rc-webhook');
 
 axios.defaults.adapter = require('axios/lib/adapters/http');
 
-describe('Notification Webhook', () => {
+describe('Notification Webhooks', () => {
   it('should get 404 without webhook uri', async () => {
     const res = await request(server).get('/webhooks/new');
     expect(res.status).toEqual(404);
@@ -219,5 +219,268 @@ describe('Notification Webhook', () => {
     await rcWebhookRecord.destroy();
     trelloBoardScope.done();
     trelloUserScope.done();
+  });
+
+  it('should get 403 when create webhook info without token', async () => {
+    const res = await request(server).post('/webhooks');
+    expect(res.status).toEqual(403);
+  });
+
+  it('should get 403 when create webhook info without rcWebhook', async () => {
+    const res = await request(server).post('/webhooks').send({
+      token: 'test_token',
+    });
+    expect(res.status).toEqual(403);
+  });
+
+  it('should get 403 when create webhook info without boardId', async () => {
+    const res = await request(server).post('/webhooks').send({
+      token: 'test_token',
+      rcWebhook: 'http://test.com/webhook/12121',
+    });
+    expect(res.status).toEqual(403);
+  });
+
+  it('should get 403 when create webhook info without filters', async () => {
+    const res = await request(server).post('/webhooks').send({
+      token: 'test_token',
+      rcWebhook: 'http://test.com/webhook/12121',
+      boardId: 'test_board_id',
+    });
+    expect(res.status).toEqual(403);
+  });
+
+  it('should get 401 when create webhook info without invalid token', async () => {
+    const res = await request(server).post('/webhooks').send({
+      token: 'test_token',
+      rcWebhook: 'http://test.com/webhook/12121',
+      boardId: 'test_board_id',
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(401);
+  });
+
+  it('should get 401 when create webhook info with no found user id in token', async () => {
+    const token = jwt.generateToken({
+      id: 'wrong_user_id',
+    });
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: 'http://test.com/webhook/12121',
+      boardId: 'test_board_id',
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(401);
+  });
+
+  it('should get 401 when create webhook info with trello user token empty', async () => {
+    const trelloUserId = 'trello_user_id';
+    const trelloUserRecord = await TrelloUser.create({
+      id: trelloUserId,
+      token: '',
+    });
+    const token = jwt.generateToken({
+      id: trelloUserId,
+    });
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: 'http://test.com/webhook/12121',
+      boardId: 'test_board_id',
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(401);
+    await trelloUserRecord.destroy();
+  });
+
+  it('should create webhook successfully at new subscription', async () => {
+    const trelloUserId = 'trello_user_id';
+    const trelloUserRecord = await TrelloUser.create({
+      id: trelloUserId,
+      token: 'test_token',
+    });
+    const token = jwt.generateToken({
+      id: trelloUserId,
+    });
+    const boardId = 'test_board_id';
+    const trelloLabelsScope = nock('https://api.trello.com')
+      .get(uri => uri.includes(`/1/boards/${boardId}/labels?`))
+      .reply(200, [
+        { "id":"6094fb83d41eeff1fa76129d", "name":"", "color":"green" },
+        { "id":"6094fb83d41eeff1fa7612a1", "name":"", "color":"yellow" },
+      ]);
+    const trelloWebhookId = 'test_webhook_id';
+    const trelloWebhooksScope = nock('https://api.trello.com')
+      .post(uri => uri.includes('/1/webhooks?'))
+      .reply(200, {
+        id: trelloWebhookId,
+      });
+    const rcWebhookId = '12121';
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: `http://test.com/webhook/${rcWebhookId}`,
+      boardId,
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(200);
+    await trelloUserRecord.destroy();
+    const rcWebhookRecord = await RCWebhook.findByPk(rcWebhookId);
+    const trelloWebhookRecord = await TrelloWebhook.findByPk(rcWebhookRecord.trello_webhook_id);
+    expect(trelloWebhookRecord.trello_webhook_id).toEqual(trelloWebhookId);
+    trelloLabelsScope.done();
+    trelloWebhooksScope.done();
+    await rcWebhookRecord.destroy();
+    await trelloWebhookRecord.destroy();
+  });
+
+  it('should create webhook successfully with existing rcWebhook record', async () => {
+    const trelloUserId = 'trello_user_id';
+    const trelloUserRecord = await TrelloUser.create({
+      id: trelloUserId,
+      token: 'test_token',
+    });
+    const rcWebhookId = '12121';
+    const rcWebhookRecord = await RCWebhook.create({
+      id: rcWebhookId,
+    });
+    const token = jwt.generateToken({
+      id: trelloUserId,
+    });
+    const boardId = 'test_board_id';
+    const trelloLabelsScope = nock('https://api.trello.com')
+      .get(uri => uri.includes(`/1/boards/${boardId}/labels?`))
+      .reply(200, [
+        { "id":"6094fb83d41eeff1fa76129d", "name":"", "color":"green" },
+        { "id":"6094fb83d41eeff1fa7612a1", "name":"", "color":"yellow" },
+      ]);
+    const trelloWebhookId = 'test_webhook_id';
+    const trelloWebhooksScope = nock('https://api.trello.com')
+      .post(uri => uri.includes('/1/webhooks?'))
+      .reply(200, {
+        id: trelloWebhookId,
+      });
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: `http://test.com/webhook/${rcWebhookId}`,
+      boardId,
+      filters: 'addChecklistToCard,commentCard',
+    });
+    expect(res.status).toEqual(200);
+    const trelloWebhookRecord = await TrelloWebhook.findByPk(rcWebhookRecord.trello_webhook_id);
+    expect(trelloWebhookRecord.trello_webhook_id).toEqual(trelloWebhookId);
+    trelloLabelsScope.done();
+    trelloWebhooksScope.done();
+    await rcWebhookRecord.destroy();
+    await trelloWebhookRecord.destroy();
+    await trelloUserRecord.destroy();
+  });
+
+  it('should create webhook successfully with existing trello webhook record', async () => {
+    const trelloUserId = 'trello_user_id';
+    const trelloUserRecord = await TrelloUser.create({
+      id: trelloUserId,
+      token: 'test_token',
+    });
+    const rcWebhookId = '12121';
+    const rcWebhookRecord = await RCWebhook.create({
+      id: rcWebhookId,
+    });
+    const boardId = 'test_board_id';
+    const oldTrelloWebhookId = 'test_trello_webhook_id';
+    let trelloWebhookRecord = await TrelloWebhook.create({
+      id: rcWebhookRecord.trello_webhook_id,
+      rc_webhook_id: `http://test.com/webhook/${rcWebhookId}`,
+      trello_user_id: trelloUserId,
+      trello_webhook_id: oldTrelloWebhookId,
+      config: {
+        boardId,
+        labels: [],
+        filters: '',
+      },
+    });
+    const token = jwt.generateToken({
+      id: trelloUserId,
+    });
+    const trelloLabelsScope = nock('https://api.trello.com')
+      .get(uri => uri.includes(`/1/boards/${boardId}/labels?`))
+      .reply(200, [
+        { "id":"6094fb83d41eeff1fa76129d", "name":"", "color":"green" },
+        { "id":"6094fb83d41eeff1fa7612a1", "name":"", "color":"yellow" },
+      ]);
+    const trelloWebhookId = 'test_webhook_id';
+    const trelloWebhooksScope = nock('https://api.trello.com')
+      .post(uri => uri.includes('/1/webhooks?'))
+      .reply(200, {
+        id: trelloWebhookId,
+      });
+    const trelloDeleteWebhooksScope = nock('https://api.trello.com')
+      .delete(uri => uri.includes(`/1/webhooks/${oldTrelloWebhookId}?`))
+      .reply(200, {
+        id: trelloWebhookId,
+      });
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: `http://test.com/webhook/${rcWebhookId}`,
+      boardId,
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(200);
+    trelloWebhookRecord = await TrelloWebhook.findByPk(rcWebhookRecord.trello_webhook_id);
+    expect(trelloWebhookRecord.trello_webhook_id).toEqual(trelloWebhookId);
+    trelloLabelsScope.done();
+    trelloWebhooksScope.done();
+    trelloDeleteWebhooksScope.done();
+    await rcWebhookRecord.destroy();
+    await trelloWebhookRecord.destroy();
+    await trelloUserRecord.destroy();
+  });
+
+  it('should response 401 when trello api return 401', async () => {
+    const trelloUserId = 'trello_user_id';
+    const trelloUserRecord = await TrelloUser.create({
+      id: trelloUserId,
+      token: 'test_token',
+    });
+    const token = jwt.generateToken({
+      id: trelloUserId,
+    });
+    const boardId = 'test_board_id';
+    const trelloLabelsScope = nock('https://api.trello.com')
+      .get(uri => uri.includes(`/1/boards/${boardId}/labels?`))
+      .reply(401, {});
+    const rcWebhookId = '12121';
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: `http://test.com/webhook/${rcWebhookId}`,
+      boardId,
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(401);
+    await trelloUserRecord.destroy();
+    trelloLabelsScope.done();
+  });
+
+  it('should response 500 when trello api return 500', async () => {
+    const trelloUserId = 'trello_user_id';
+    const trelloUserRecord = await TrelloUser.create({
+      id: trelloUserId,
+      token: 'test_token',
+    });
+    const token = jwt.generateToken({
+      id: trelloUserId,
+    });
+    const boardId = 'test_board_id';
+    const trelloLabelsScope = nock('https://api.trello.com')
+      .get(uri => uri.includes(`/1/boards/${boardId}/labels?`))
+      .reply(500, {});
+    const rcWebhookId = '12121';
+    const res = await request(server).post('/webhooks').send({
+      token,
+      rcWebhook: `http://test.com/webhook/${rcWebhookId}`,
+      boardId,
+      filters: 'addChecklistToCard',
+    });
+    expect(res.status).toEqual(500);
+    await trelloUserRecord.destroy();
+    trelloLabelsScope.done();
   });
 });
