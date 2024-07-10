41d807fae28bd74a0ef655e80a9281a6dd1fa32f
Embbnux Ji <embbnux.ji@ringcentral.com>
2024-07-10 17:53:49 +0800

    misc: fix wrong domain https in csp (#96)

--- src/app/lib/constants.js
+++ src/app/lib/constants.js
@@ -1,7 +1,7 @@
 const ICON_URL = 'https://raw.githubusercontent.com/ringcentral/trello-notification-app/main/icons/trello-legacy.png';
 const COLOR_IMAGE_URL_BASE = 'https://raw.githubusercontent.com/ringcentral/trello-notification-app/main/icons/';
 const DIALOG_ICON_URL = 'https://raw.githubusercontent.com/ringcentral/trello-notification-app/main/icons/trello.png';
-const IFRAME_HOST_DOMAINS = "https://*.ringcentral.com https://*.ringcentral.biz https://*.glip.com https://*.glip.net https://glip.com https://*.labs.ringcentral.com http://*.integration.ringcentral.com http://*.devtest.ringcentral.com https://*.unifyoffice.com https://*.officeathand.att.com https://*.cloudoffice.avaya.com https://*.cloudwork.bt.com https://*.rainbowoffice.com https://*.businessconnect.telus.com https://*.vodafonebusiness.ringcentral.com";
+const IFRAME_HOST_DOMAINS = "https://*.ringcentral.com https://*.ringcentral.biz https://*.glip.com https://*.glip.net https://glip.com https://*.labs.ringcentral.com https://*.integration.ringcentral.com https://*.devtest.ringcentral.com https://*.unifyoffice.com https://*.officeathand.att.com https://*.cloudoffice.avaya.com https://*.cloudwork.bt.com https://*.rainbowoffice.com https://*.businessconnect.telus.com https://*.vodafonebusiness.ringcentral.com";
 
 exports.ICON_URL = ICON_URL;
 exports.COLOR_IMAGE_URL_BASE = COLOR_IMAGE_URL_BASE;
